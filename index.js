827c2fccea1995d5a8ac8aa46c78139fb8658ac7
Mohammed Amine Elazzab <ma.elazzab@gmail.com>
2022-02-07 10:46:08 +0100

    js

--- index.js
+++ index.js
@@ -11,10 +11,67 @@
 
 
 class Node {
-    constructor(data, next = null){
-        this.data= data;
-        this.next= next;
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
     }
 }
 
-const n1 = new Node(100);
+// const n1 = new Node(100);
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    //Insert first node
+    insertFirst(data) {
+        this.head = new Node(data, this.head);
+        this.size++;
+    }
+
+    //Insert last node
+    insertLast(data) {
+            let node = new Node(data);
+            let current;
+
+            //if empty make head
+            if (!this.head) {
+                this.head = node;
+
+            } else {
+                current = this.head;
+
+                while (current.next) {
+                    current = current.next;
+                }
+                current.next = node;
+            }
+            this.size++;
+        }
+        //Insert at index
+
+    //Get at index
+
+    //Remove at index
+
+    //Clear List
+
+    //Print list data
+    printListData() {
+        let current = this.head;
+
+        while (current) {
+            console.log(current.next);
+            current = current.next;
+        }
+    }
+}
+const ll = new LinkedList();
+ll.insertFirst(100);
+ll.insertFirst(200);
+ll.insertFirst(300);
+ll.insertLast(400);
+// console.log(ll);
+ll.printListData();
