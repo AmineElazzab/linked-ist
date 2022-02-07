7ae627038ab2ec2da40f9c45bf89cf9ff3da3c28
Mohammed Amine Elazzab <ma.elazzab@gmail.com>
2022-02-07 09:54:23 +0100

    js

---
+++ index.js
@@ -0,0 +1,20 @@
+// const n1 = {
+//     data: 100
+// };
+
+// const n2 ={
+//     data: 200
+// };
+
+// n1.next = n2;
+// console.log(n1);
+
+
+class Node {
+    constructor(data, next = null){
+        this.data= data;
+        this.next= next;
+    }
+}
+
+const n1 = new Node(100);
